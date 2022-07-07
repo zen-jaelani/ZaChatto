78ae7978cb26091a272c28e50b6e5cbb77d0965c
Qxtlp <xi.tulop@yandex.com>
2022-07-07 22:25:20 +0700

    add auth layout

---
+++ components/layout/auth.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+type Props = {
+  children?: JSX.Element | JSX.Element[];
+  title: string;
+};
+
+export default function Auth({ children, title }: Props) {
+  return (
+    <div className="flex bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 h-screen">
+      <div className="bg-slate-800 h-full md:h-fit w-full md:w-1/2 m-auto text-white py-5 rounded-md">
+        <h1 className="text-center text-3xl font-bold mb-5">{title}</h1>
+        <hr className="border-pink-400 border-b-2" />
+
+        {children}
+
+        <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t-2 border-pink-400"></div>
+          <span className="flex-shrink mx-4 mb-1 text-white text-2xl">or</span>
+          <div className="flex-grow border-t-2 border-pink-400"></div>
+        </div>
+        <div className="text-center my-3">
+          {title === "Login" ? (
+            <p>
+              Need an account?{" "}
+              <Link href={"/auth/register"}>
+                <span className="text-pink-400 cursor-pointer">Register</span>
+              </Link>
+            </p>
+          ) : (
+            <p>
+              Already have an account?{" "}
+              <Link href={"/auth/login"}>
+                <span className="text-pink-400 cursor-pointer">Login</span>
+              </Link>
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
