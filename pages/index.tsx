ced5ebf669300082726446b9e6865d814864e32e
Qxtlp <xi.tulop@yandex.com>
2022-07-16 22:20:09 +0700

    add chat feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,116 +1,218 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { MdSearch } from "react-icons/md";
+import { IoImageOutline } from "react-icons/io5";
 import { GiBigGear, GiCommercialAirplane } from "react-icons/gi";
+import { SiApacheairflow } from "react-icons/si";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db, storage } from "../firebaseconfig";
+import SideBar from "../components/sidebar";
+import { useRouter } from "next/router";
+import {
+  addDoc,
+  collection,
+  DocumentData,
+  onSnapshot,
+  orderBy,
+  query,
+  Timestamp,
+} from "firebase/firestore";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import Moment from "react-moment";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const scrollRef = useRef<null | HTMLDivElement>(null);
+  const [activeUser, setActiveUser] = useState<DocumentData | null>(null);
+  const [message, setMessage] = useState("");
+  const [conversation, setConversation] = useState<DocumentData[]>([]);
+  const [imgInput, setImgInput] = useState<File | null | undefined>(null);
+  const [user, loading, error] = useAuthState(auth);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [conversation]);
+
+  if (loading) {
+    return (
+      <div className="w-full h-screen bg-slate-800 flex items-center">
+        <SiApacheairflow className="text-9xl text-pink-400 mx-auto animate-spin" />
+      </div>
+    );
+  }
+
+  if (!user) {
+    router.push("auth/login");
+  }
+
+  const setChat = (data: DocumentData) => {
+    try {
+      console.log("get chat data start.....");
+      setActiveUser(data);
+      if (!user?.uid) return;
+      const user1 = user?.uid;
+      const user2 = data?.uid;
+      const id = user1 > user2 ? `${user1}<>${user2}` : `${user2}<>${user1}`;
+      const msgRef = collection(db, "messages", id, "chat");
+      const q = query(msgRef, orderBy("createdAt", "asc"));
+      onSnapshot(q, (querSnapshot) => {
+        let msgs: DocumentData[] = [];
+        querSnapshot.forEach((doc) => {
+          msgs.push(doc.data());
+        });
+        setConversation(msgs);
+      });
+      console.log("get chat data finish");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  console.log(conversation);
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    if (auth.currentUser?.uid && (message || imgInput)) {
+      console.log("send message start....");
+
+      let url: string | undefined;
+      if (imgInput) {
+        const imgRef = ref(
+          storage,
+          `images/${new Date().getTime()} - ${imgInput.name}`
+        );
+        const snap = await uploadBytes(imgRef, imgInput);
+        const dlUrl = await getDownloadURL(ref(storage, snap.ref.fullPath));
+        url = dlUrl;
+      }
+      const user1 = auth.currentUser?.uid;
+      const user2 = activeUser?.uid;
+      const id = user1 > user2 ? `${user1}<>${user2}` : `${user2}<>${user1}`;
+      await addDoc(collection(db, "messages", id, "chat"), {
+        message: message || null,
+        from: user1,
+        to: user2,
+        createdAt: Timestamp.fromDate(new Date()),
+        media: url || null,
+      });
+      setMessage("");
+      console.log("send message finish!");
+    }
+  };
+
   return (
     <div className="flex">
-      <div className="bg-pink-400 text-white h-screen w-full md:w-1/3 lg:w-1/5 p-5 pr-0 relative">
-        <div className="relative h-48 -ml-5 -mt-5 ">
-          <Image
-            src={"/ZaLogo.png"}
-            layout="fill"
-            alt="ZaChatto"
-            objectFit="contain"
-            className=""
-          />
-        </div>
-        {/* <h1 className="text-3xl font-bold subpixel-antialiased logo ">
-          ザChatto
-        </h1> */}
+      <SideBar setChat={setChat} />
 
-        <div className="mt-5">
-          <div className="pr-5">
-            <h1 className="font-bold text-2xl">Messages</h1>
-            <label className="relative block my-10">
-              <span className="sr-only">Search</span>
-              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <MdSearch color="black" size={25} />
-              </span>
+      <main className="bg-zinc-900 max-h-screen hidden overflow-hidden  md:flex md:w-2/3 lg:w-4/5 relative">
+        {activeUser ? (
+          <>
+            <div
+              className="text-white border-b border-pink-400 flex fixed top-0 p-5 bg-zinc-900 z-50"
+              style={{ width: "inherit" }}
+            >
+              <div className={`w-16 h-14 relative `}>
+                {activeUser?.image ? (
+                  <Image
+                    src={activeUser.image}
+                    alt="profile"
+                    layout="fill"
+                    objectFit="cover"
+                    className={"rounded-full"}
+                  />
+                ) : (
+                  <div className="bg-pink-400 w-full h-full text-3xl rounded-full flex flex-col justify-center items-center">
+                    {activeUser?.username?.split?.(" ")[0].slice(0, 1)}
+                  </div>
+                )}
+              </div>
+
+              <div className="flex items-center justify-between w-full">
+                <div className="ml-3 flex flex-col justify-center">
+                  <h1>{activeUser.username}</h1>
+                  <p className="">{activeUser.email}</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="py-24 px-5 overflow-auto w-full">
+              {conversation.length ? (
+                conversation.map((msg, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className={`relative flex flex-col ${
+                        msg.from === user?.uid ? "items-end " : ""
+                      }`}
+                      ref={scrollRef}
+                    >
+                      <div
+                        className={`w-fit h-fit py-2 px-5 flex flex-col mt-3 ${
+                          msg.from === user?.uid
+                            ? "bg-pink-400 mr-3 rounded-l-lg rounded-t-lg text-white"
+                            : "bg-white rounded-r-lg rounded-t-lg ml-3"
+                        } `}
+                      >
+                        {msg.media ? (
+                          <div className="my-2">
+                            <Image
+                              src={msg.media}
+                              width={100}
+                              height={100}
+                              objectFit="cover"
+                              alt=""
+                              className="rounded-lg "
+                            />
+                          </div>
+                        ) : null}
+                        {msg.message}
+                      </div>
+                      <small className="text-white mx-3">
+                        <Moment fromNow>{msg.createdAt.toDate()}</Moment>
+                      </small>
+                    </div>
+                  );
+                })
+              ) : (
+                <div></div>
+              )}
+            </div>
+
+            <div
+              className="border-t border-pink-400 flex bg-zinc-900 max-w-full px-15 fixed bottom-0 py-4 px-7"
+              style={{ width: "inherit" }}
+            >
+              <label htmlFor="imageMessage">
+                <IoImageOutline color="white" size={40} className="mr-5 mt-3" />
+              </label>
               <input
-                className="placeholder:italic placeholder:text-slate-400 block bg-white text-black w-full rounded-md py-2 pl-9 pr-3 focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                placeholder="Search for anything..."
-                type="text"
-                name="search"
+                type="file"
+                id="imageMessage"
+                className="hidden"
+                accept="image/*"
+                onChange={(e) => setImgInput(e.target.files?.[0])}
               />
-            </label>
-          </div>
+              <input
+                type="text"
+                className="rounded-lg p-3 w-full"
+                value={message}
+                placeholder={`Message ${activeUser.username}`}
+                onChange={(e) => setMessage(e.target.value)}
+              />
 
-          <div className="flex max-w-full">
-            <Image
-              src={require("/public/vercel.svg")}
-              alt="profile"
-              height={"70"}
-              width={"70"}
-              objectFit="contain"
-              className={"bg-white rounded-full flex-none"}
-            />
-
-            <div className="flex-initial w-2/3 mx-2 flex flex-col justify-center">
-              <h1>Nano nano</h1>
-              <p className="truncate">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              </p>
+              <GiCommercialAirplane
+                color="white"
+                size={35}
+                className="my-3 mx-8 cursor-pointer"
+                onClick={handleSubmit}
+              />
             </div>
-            <p className="text-sm mt-2">Dec 10</p>
+          </>
+        ) : (
+          <div className="text-pink-400 text-2xl text-center pt-10 w-full">
+            Select a user to start conversation
           </div>
-        </div>
-
-        <div
-          className="bg-pink-500 flex max-w-full px-15 fixed bottom-0 p-5 -ml-5"
-          style={{ width: "inherit" }}
-        >
-          <Image
-            src={require("/public/vercel.svg")}
-            alt="profile"
-            height={"70"}
-            width={"70"}
-            objectFit="contain"
-            className={"bg-white rounded-full flex-none"}
-          />
-
-          <div className="flex items-center justify-between w-full">
-            <div className="ml-3 flex flex-col justify-center">
-              <h1>Nano mano</h1>
-              <p className="">nano@mano.com</p>
-            </div>
-            <GiBigGear size={35} className="-ml-0" />
-          </div>
-        </div>
-      </div>
-
-      <main className="bg-zinc-900 h-screen md:w-2/3 lg:w-4/5 relative">
-        <div
-          className="text-white border-b border-pink-400 flex fixed top-0 p-5"
-          style={{ width: "inherit" }}
-        >
-          <Image
-            src={require("/public/vercel.svg")}
-            alt="profile"
-            height={"70"}
-            width={"70"}
-            objectFit="contain"
-            className={"bg-white rounded-full flex-none"}
-          />
-
-          <div className="flex items-center justify-between w-full">
-            <div className="ml-3 flex flex-col justify-center">
-              <h1>Nano mano</h1>
-              <p className="">nano@mano.com</p>
-            </div>
-          </div>
-        </div>
-
-        <div
-          className="border-t border-pink-400 flex  max-w-full px-15 fixed bottom-0 py-4 px-7"
-          style={{ width: "inherit" }}
-        >
-          <input type="text" className="rounded-lg p-3 w-full" />
-
-          <GiCommercialAirplane color="white" size={35} className="my-3 mx-8" />
-        </div>
+        )}
       </main>
     </div>
   );
