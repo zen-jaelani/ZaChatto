98f9bf31153329a9affcd0b2fcd264878a07c8d7
Qxtlp <xi.tulop@yandex.com>
2022-07-18 19:34:28 +0700

    fix ui

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,6 +1,7 @@
 import { doc, DocumentData, onSnapshot } from "firebase/firestore";
 import Image from "next/image";
 import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { IoImageOutline } from "react-icons/io5";
 import Moment from "react-moment";
 import { auth, db } from "../firebaseconfig";
 
@@ -21,18 +22,24 @@ export default function Contact({ data }: Props) {
 
   return (
     <div className="grid grid-cols-4 cursor-pointer pl-5 pr-2 py-3 -ml-5 hover:bg-pink-500">
-      <div className="">
-        <Image
-          src={require("/public/vercel.svg")}
-          alt="profile"
-          height={"70"}
-          width={"70"}
-          objectFit="contain"
-          className={"bg-white rounded-full"}
-        />
+      <div className="relative flex  items-center ">
+        {data?.image ? (
+          <Image
+            src={data.image}
+            alt="profile"
+            height={"64"}
+            width={"64"}
+            objectFit="cover"
+            className={"rounded-full"}
+          />
+        ) : (
+          <div className="bg-slate-800 w-16 h-16 text-3xl rounded-full flex flex-col justify-center items-center">
+            {data?.username?.split?.(" ")[0].slice(0, 1)}
+          </div>
+        )}
       </div>
 
-      <div className="col-start-2 col-end-6 mx-2 flex flex-col justify-center ">
+      <div className="col-start-2 col-end-5 -ml-5 md:ml-0 mx-2 flex flex-col justify-center ">
         <div className="flex justify-between">
           <h1>{data.username}</h1>
           <p className="text-sm w-fit">
@@ -41,8 +48,14 @@ export default function Contact({ data }: Props) {
             )}
           </p>
         </div>
-        <p className="truncate">
+        <p className="truncate flex">
           {auth.currentUser?.uid === last?.from && <strong>me: </strong>}
+          {!last?.message && last?.media && (
+            <div className="flex">
+              <IoImageOutline color="white" size={25} className="" />
+              Image
+            </div>
+          )}
           {last?.message}
         </p>
       </div>
